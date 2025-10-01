9a7c50017680dd5e5e60b89279490f6014d4e6e4
zmigd <zmigdalovich@gmail.com>
2025-10-01 14:16:27 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: command === 'serve' ? '/' : '/Html-project/', // <-- добавлено
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
